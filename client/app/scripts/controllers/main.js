9cf602d7f58273d9c62e4ca3ce5dc2142455185e
ignacio donoso <naxonicolas1997@gmail.com>
2018-12-20 15:09:00 -0300

    Proyect base
    Creation of the base of the system

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ecocvApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ecocvApp
+ */
+angular.module('ecocvApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
