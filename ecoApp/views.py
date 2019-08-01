1b1cda57e6fdf2657c016f5b0b0e6d719c82211c
iDonosof <naxonicolas1997@gmail.com>
2019-07-31 21:53:26 -0400

    Estilo nav bar

--- ecoApp/views.py
+++ ecoApp/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def event_information(request):
     return render(request, 'ecoApp/event_information.html',{})
+
+def login(request):
+    return render(request, 'ecoApp/login.html', {})
+
+def signin(request):
+    return render(request, 'ecoApp/signin.html', {})
+
+def curriculum(request):
+    return render(request, 'ecoApp/curriculum.html', {})
+
+def profile(request):
+    return render(request, 'ecoApp/profile.html', {})
